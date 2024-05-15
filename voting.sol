c4b0c4c0baa3073e9ee3215476e126ce5409fbc0
João Pedro Lourenço Affonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2024-05-15 19:42:44 -0300

    Update voting.sol
    Novas funções e um evento para avisar que a votação já acabou

--- voting.sol
+++ voting.sol
@@ -3,34 +3,44 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "hardhat/console.sol";
 
 contract Voting is Ownable {
     constructor(address initialOwner)
         Ownable(initialOwner)
     {}
 
+    // Variaveis e eventos
     address[] internal eleitores;
-    bool internal paused = false;
+    bool internal isPaused = false;
     mapping(string => string) public votacoes;
+    mapping(string => string) public listaDeVotacoes;
+    string[] internal listaDeNomesDeVotacoes;
     mapping(string => uint256[]) public votos;
+    event fimDeVotacao(string indexed nomeDaVotacao, string resultado);
 
     function pause() public onlyOwner {
-        paused = true;
+        isPaused = true;
     }
 
     function unpause() public onlyOwner {
-        paused = false;
+        isPaused = false;
+    }
+
+    function paused() public view returns (bool) {
+        return isPaused;
     }
 
     function adicionaEleitor(address eleitor) external onlyOwner {
-        require(!paused, "Contrato pausado.");
-        bool x = isEleitor(eleitor);
-        require(!x, "Eleitor ja cadastrado.");
+        require(!isPaused, "Contrato pausado.");
+        //bool x = isEleitor(eleitor);
+        require(!isEleitor(eleitor), "Eleitor ja cadastrado.");
         eleitores.push(eleitor);
     }
 
     function excluiEleitor(address eleitor) external onlyOwner {
-        require(!paused, "Contrato pausado.");
+        require(!isPaused, "Contrato pausado.");
         
         for (uint256 i = 0; i < eleitores.length - 1; i++) {
             if (eleitores[i] == eleitor) {
@@ -42,17 +52,17 @@ contract Voting is Ownable {
     }
 
     function retornaEleitores() public view returns (address[] memory) {
-        require(!paused, "Contrato pausado.");
+        require(!isPaused, "Contrato pausado.");
         return eleitores;
     }
 
     function numeroDeEleitores() public view returns (uint256) {
-        require(!paused, "Contrato pausado.");
+        require(!isPaused, "Contrato pausado.");
         return eleitores.length;
     }
 
     function isEleitor(address eleitor) public view returns (bool) {
-        require(!paused, "Contrato pausado.");
+        require(!isPaused, "Contrato pausado.");
         if (eleitores.length == 0) {
             return false;
         }
@@ -64,21 +74,46 @@ contract Voting is Ownable {
         return false;
     }
 
-    function cadastrarVotacao(string memory nomeDaVotacao, string memory status) external onlyOwner {
-        require(!paused, "Contrato pausado.");
+    function cadastrarVotacao(string memory nomeDaVotacao, string memory status, string memory detalhes) external onlyOwner {
+        require(!isPaused, "Contrato pausado.");
         require(bytes(votacoes[nomeDaVotacao]).length == 0, "Votacao ja existe");
         votacoes[nomeDaVotacao] = status;
         votos[nomeDaVotacao].push(0);
         votos[nomeDaVotacao].push(0);
+        listaDeVotacoes[nomeDaVotacao] = detalhes;
+        listaDeNomesDeVotacoes.push(nomeDaVotacao);
+    }
+
+    function editaStatusDeVotacao (string memory nomeDaVotacao, string memory status) external onlyOwner {
+        require(!isPaused, "Contrato pausado.");
+        require(bytes(votacoes[nomeDaVotacao]).length >= 0, "Votacao nao existe");
+        votacoes[nomeDaVotacao] = status;
+    }
+
+    function editaDetalhesDeVotacao (string memory nomeDaVotacao, string memory status) external onlyOwner {
+        require(!isPaused, "Contrato pausado.");
+        require(bytes(votacoes[nomeDaVotacao]).length >= 0, "Votacao nao existe");
+        listaDeVotacoes[nomeDaVotacao] = detalhes;
     }
 
     function statusDeVotacao(string memory nomeDaVotacao) public view returns (string memory) {
-        require(!paused, "Contrato pausado.");
+        require(!isPaused, "Contrato pausado.");
+        require(bytes(votacoes[nomeDaVotacao]).length >= 0, "Votacao nao existe");
         return votacoes[nomeDaVotacao];
     }
 
+    function retornaVotacoes() public view returns (string memory) {
+        string memory stringListaDeVotacoes = "";
+
+        for (uint256 i = 0; i <= listaDeNomesDeVotacoes.length - 1; i++) {
+            stringListaDeVotacoes = string.concat(stringListaDeVotacoes, "\n ", listaDeNomesDeVotacoes[i]," - " , listaDeVotacoes[listaDeNomesDeVotacoes[i]]);
+        }
+        
+        return stringListaDeVotacoes;
+    }
+
     function votosAtual(string memory nomeDaVotacao) public view returns (uint256[2] memory) {
-        require(!paused, "Contrato pausado.");
+        require(!isPaused, "Contrato pausado.");
         require(bytes(votacoes[nomeDaVotacao]).length > 0, "Votacao nao existe");
         require(isEleitor(msg.sender),"Eleitor nao cadastrado.");
 
@@ -88,16 +123,25 @@ contract Voting is Ownable {
     }
 
     function votar(string memory nomeDaVotacao, uint256 voto) public {
-        require(!paused, "Contrato pausado.");
+        require(!isPaused, "Contrato pausado.");
         require(bytes(votacoes[nomeDaVotacao]).length > 0, "Votacao nao existe");
         require(isEleitor(msg.sender),"Eleitor nao cadastrado.");
         require(voto == 1 || voto == 0, "Voto invalido.");
+        require(2 * votos[nomeDaVotacao][0] <= eleitores.length || 2 * votos[nomeDaVotacao][1] <= 2 * eleitores.length, votacoes[nomeDaVotacao]);
         
         //0 para NÃO e 1 para SIM
         if (voto == 0) {
             votos[nomeDaVotacao][0] += 1;
         } else if (voto == 1) {
             votos[nomeDaVotacao][1] += 1;
-        }     
+        }
+
+        if (2 * votos[nomeDaVotacao][0] >= eleitores.length ) {
+            emit fimDeVotacao(nomeDaVotacao, "Nao");
+            votacoes[nomeDaVotacao] = "Votacao encerrada, resultado: 'Nao'";
+        } else if (2 * votos[nomeDaVotacao][1] >= eleitores.length ) {
+            emit fimDeVotacao(nomeDaVotacao, "Sim");
+            votacoes[nomeDaVotacao] = "Votacao encerrada, resultado: 'Sim'";
+        }
     }
 }
